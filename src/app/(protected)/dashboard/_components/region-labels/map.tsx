6296906cb091781902d4e59acc48a1cfcc88c095
Danny <896547@senati.pe>
2026-02-04 14:12:08 -0500

    Avance de dash

---
+++ src/app/(protected)/dashboard/_components/region-labels/map.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useEffect } from "react";
+import jsVectorMap from "jsvectormap";
+import "jsvectormap/dist/css/jsvectormap.css";
+
+export default function Map() {
+  useEffect(() => {
+    new jsVectorMap({
+      selector: "#mapOne",
+      map: "us_aea",
+      zoomButtons: true,
+      regionStyle: {
+        initial: {
+          fill: "#C8D0D8",
+        },
+        hover: {
+          fillOpacity: 1,
+          fill: "#3056D3",
+        },
+      },
+      regionLabelStyle: {
+        initial: {
+          fontWeight: "semibold",
+          fill: "#fff",
+        },
+        hover: {
+          cursor: "pointer",
+        },
+      },
+      labels: {
+        regions: {
+          render(code: string) {
+            return code.split("-")[1];
+          },
+        },
+      },
+    });
+  }, []);
+
+  return (
+    <div className="h-[422px]">
+      <div id="mapOne" className="mapOne map-btn h-full w-full" />
+    </div>
+  );
+}
